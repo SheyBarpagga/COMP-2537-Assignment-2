d4dbbd48db5d4d36faf700eccc1ef4d5188c1b54
Shey Barpagga <sheybarpagga@gmail.com>
2022-05-13 20:07:36 -0700

    working on poke API

--- server.js
+++ server.js
@@ -56,27 +56,30 @@ const timeLineModel = mongoose.model("times", TimesSchema);
 
 const pokeModel = mongoose.model("pokemon", PokeSchema);
 
-pokeModel.find().forEach(function(data) {
-  app.get(`/pokemon/getPoke/${data.id}`, function(req, res) {
-    res.send(JSON.stringify(data));
-  })
+app.get('/pokemon/getPoke', function(req, res) {
+
+  pokeModel.find({}, function(err, data){
+      if (err){
+        console.log("Error " + err);
+      }else{
+        console.log("Data "+ JSON.stringify(data) );
+      }
+      res.send(JSON.stringify(data));
+  });
 })
+for (var x = 1; x < 31; x++) {
+  app.get(`/pokemon/getPoke/${x}`, function(req, res) {
 
-
-// for (var x = 1; x < 31; x++) {
-//   app.get(`/pokemon/getPoke/${x}`, function(req, res) {
-
-//     pokeModel.find({'id': `${x}`}, function(err, data){
-//         if (err){
-//           console.log("Error " + err);
-//         }else{
-//           console.log("Data "+ JSON.stringify(data) );
-//         }
-//         res.send(JSON.stringify(data));
-//     });
-//   })
-// }
-
+    pokeModel.find({id: `${x}`}, function(err, data){
+        if (err){
+          console.log("Error " + err);
+        }else{
+          console.log("Data "+ JSON.stringify(data) );
+        }
+        res.send(JSON.stringify(data));
+    });
+  })
+}
 app.get('/times/getAllEvents', function(req, res) {
 
   timeLineModel.find({}, function(err, data){
